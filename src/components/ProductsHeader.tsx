59d7bb9d1de1388128d1f1689fbdf55e001de942
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-05 04:44:01 +0100

    Fix Responsiveness for ProductsHeader

--- src/components/ProductsHeader.tsx
+++ src/components/ProductsHeader.tsx
@@ -4,7 +4,7 @@ import SearchProducts from "./SearchProducts";
 
 const ProductsHeader = () => {
   return (
-    <header className="pt-60 md:px-10 px-5 flex justify-between gap-y-7 lg:flex-row flex-col lg:items-stretch items-center">
+    <header className="md:pt-60 pt-44 md:px-10 px-5 flex justify-between gap-y-7 lg:flex-row flex-col lg:items-stretch items-center">
       <Categories />
 
       <SearchProducts />
