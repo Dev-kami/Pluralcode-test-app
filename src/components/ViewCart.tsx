1f83e2c1f14246327c6593bf133fa059c55ef885
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-06 14:08:10 +0100

    Fix ViewCart spacing

--- src/components/ViewCart.tsx
+++ src/components/ViewCart.tsx
@@ -16,26 +16,28 @@ const ViewCart = () => {
       {!carts.length ? (
         <p className="text-center mb-[15.7rem]">Your Cart is Empty</p>
       ) : (
-        carts.map((cart) => (
-          <div
-            key={cart.id}
-            className="border border-black p-10 mx-10 flex md:flex-row flex-col items-center gap-10"
-          >
-            <div>
-              <Image
-                src={cart.image}
-                alt={cart.title}
-                width={250}
-                height={250}
-              />
+        <div className="flex flex-col space-y-5">
+          {carts.map((cart) => (
+            <div
+              key={cart.id}
+              className="border border-black p-10 mx-10 flex md:flex-row flex-col items-center gap-10"
+            >
+              <div>
+                <Image
+                  src={cart.image}
+                  alt={cart.title}
+                  width={250}
+                  height={250}
+                />
+              </div>
+              <div className="space-y-4">
+                <h1 className="font-extrabold text-xl">{cart.title}</h1>
+                <p>Price: ${cart.price}</p>
+                <p>Quantity: {quantity}</p>
+              </div>
             </div>
-            <div className="space-y-4">
-              <h1 className="font-extrabold text-xl">{cart.title}</h1>
-              <p>Price: ${cart.price}</p>
-              <p>Quantity: {quantity}</p>
-            </div>
-          </div>
-        ))
+          ))}
+        </div>
       )}
 
       <h2
