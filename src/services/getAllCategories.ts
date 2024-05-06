57fa227e9fe1c01cc777e36395b21a08290417d6
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-06 14:02:07 +0100

    Delete unused file

--- src/services/getAllCategories.ts
+++
@@ -1,7 +0,0 @@
-export async function getAllCategories() {
-  const res = await fetch("https://fakestoreapi.com/products/categories");
-
-  if (!res.ok) throw new Error("Failed to fetch categories");
-
-  return res.json();
-}
--- src/services/getProductsByCategories.ts
+++
@@ -1,7 +0,0 @@
-export async function getAllProductsByCategories(categories: string) {
-  const res = await fetch(`https://fakestoreapi.com/products/${categories}`);
-
-  if (!res.ok) throw new Error("Failed to fetch categories");
-
-  return res.json();
-}
