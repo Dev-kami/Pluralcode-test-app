79265261a08984e186797d054d0fe6d2fcfa6ead
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-07 16:53:29 +0100

    Spacing: Adjust product header margin

--- src/components/ProductsHeader.tsx
+++ src/components/ProductsHeader.tsx
@@ -14,7 +14,7 @@ const ProductsHeader = ({
   isActive: number;
 }) => {
   return (
-    <header className="md:pt-60 pt-44 md:px-10 px-5 flex justify-between gap-y-7 lg:flex-row flex-col lg:items-stretch items-center">
+    <header className="md:pt-60 pt-48 md:px-10 px-5 flex justify-between gap-y-7 lg:flex-row flex-col lg:items-stretch items-center">
       <Categories onFilterByCategory={onFilterByCategory} isActive={isActive} />
 
       <SearchProducts query={query} setQuery={setQuery} />
