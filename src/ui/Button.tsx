d84204856b30274f23563401516ad68038d62b69
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-29 09:48:25 +0100

    Add spinner to resuable button

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import React from "react";
+import MiniSpinner from "./MiniSpinner";
 
 type ButtonProps = {
   children: React.ReactNode;
   variant?: "primary" | "default" | "secondary";
   className?: string;
   href?: string;
+  loading?: boolean;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = ({
@@ -16,18 +18,22 @@ const Button = ({
   ...rest
 }: ButtonProps) => {
   const baseClasses = "border border-stone-500 py-2 px-2 text-sm";
+  const loadingClasses = "flex items-center gap-x-3";
   const variantClasses = {
     primary: "rounded-md bg-primary text-white",
     secondary: "bg-primary text-white",
     default: "rounded-md",
   };
 
-  const combinedClasses = `${baseClasses} ${variantClasses[variant]} ${className}`;
+  const combinedClasses = `${baseClasses} ${
+    variantClasses[variant]
+  } ${className} ${rest.loading && loadingClasses}`;
 
   if (href) {
     return (
       <Link href={href} className={combinedClasses}>
         {children}
+        {rest.loading && <MiniSpinner />}
       </Link>
     );
   }
@@ -35,6 +41,7 @@ const Button = ({
   return (
     <button {...rest} className={combinedClasses}>
       {children}
+      {rest.loading && <MiniSpinner />}
     </button>
   );
 };
