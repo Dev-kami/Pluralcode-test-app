996888d5d2ec2aa4169e757db03b1d0cc1a62a3e
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-12 17:51:05 +0100

    chore: Add auth types

---
+++ src/types/auth.ts
@@ -0,0 +1,28 @@
+export interface LoginInput {
+    email: string;
+    password: string;
+}
+
+export interface SignupInput {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
+
+export interface IUser {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    role: ["user" | "admin"];
+    __v: number;
+}
+
+export interface AuthResponse {
+    status: string;
+    token: string;
+    data: {
+        user: IUser;
+    };
+}
