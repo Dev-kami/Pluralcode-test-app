fc0d7fc38ca6a43afa53723209a2a6127c669d5c
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-12 17:50:43 +0100

    Update: Add an env for getting fakestore url

--- src/services/getSinglePost.ts
+++ src/services/getSinglePost.ts
@@ -1,5 +1,5 @@
 export async function getSingleProduct(productId: number) {
-    const res = await fetch(`https://fakestoreapi.com/products/${productId}`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_FAKESTORE_API}/products/${productId}`);
 
     if (!res.ok) throw new Error("Failed to get this product");
 
