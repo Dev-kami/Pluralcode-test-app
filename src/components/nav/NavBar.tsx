fd928c46302b71b4c66cf53f878557b5497108e5
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-17 14:06:31 +0100

    Wrap Navbar with UserProvider

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import NavFirstSlice from "./NavFirstSlice";
 import NavSecondSlice from "./NavSecondSlice";
 import NavThirdSlice from "./NavThirdSlice";
+import { UserProvider } from "@/context/UserContext";
 
 const NavBar = () => {
     return (
-        <nav className="sticky top-0 w-full pb-5 bg-white z-50">
-            <NavFirstSlice />
-            <NavSecondSlice />
-            <NavThirdSlice />
-        </nav>
+        <UserProvider>
+            <nav className="sticky top-0 w-full pb-5 bg-white z-50">
+                <NavFirstSlice />
+                <NavSecondSlice />
+                <NavThirdSlice />
+            </nav>
+        </UserProvider>
     );
 };
 
