447b55c9dbe0bca5aedf72a64eef4c02ba0554c1
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-17 14:07:57 +0100

    Update: Conditionally render user name if the user is logged in and handled logout onclick

--- src/components/nav/NavThirdSlice.tsx
+++ src/components/nav/NavThirdSlice.tsx
@@ -1,18 +1,29 @@
 "use client";
 
 import React from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import Link from "next/link";
 
+import { TbLogout } from "react-icons/tb";
+
 import Button from "@/ui/Button";
+import { useUser } from "@/context/UserContext";
 
 const NavThirdSlice = () => {
     const pathname = usePathname();
+    const router = useRouter();
+    const { user } = useUser();
 
     const navData = [
         { title: "Home", link: "/" },
         { title: "About", link: "/about" },
     ];
+
+    function handleLogout() {
+        localStorage.removeItem("token");
+        router.push("/login");
+    }
+
     return (
         <div className="md:px-10 px-5 pt-6 pb-3 border-b border-b-stone-400 flex items-center justify-between">
             <ul className="flex space-x-7">
@@ -30,12 +41,28 @@ const NavThirdSlice = () => {
                 ))}
             </ul>
 
-            <div className="flex space-x-5">
-                <Button href="/login">Login</Button>
-                <Button variant="primary" href="/signup">
-                    Sign up
-                </Button>
-            </div>
+            {user?.status ? (
+                <p>
+                    Welcome{" "}
+                    <span>
+                        <span className="text-primary">
+                            {user.data.user.firstName} {user.data.user.lastName}
+                        </span>
+
+                        <span className="text-stone-400"> | </span>
+                        <Button onClick={handleLogout} variant="primary">
+                            <TbLogout />
+                        </Button>
+                    </span>
+                </p>
+            ) : (
+                <div className="flex space-x-5">
+                    <Button href="/login">Login</Button>
+                    <Button variant="primary" href="/signup">
+                        Sign up
+                    </Button>
+                </div>
+            )}
         </div>
     );
 };
